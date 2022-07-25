0709bbb8d44560a91181c266d8216f6ee6f4bb26
EarthlyEric6 <minecaeft.creeper.eric@gmail.com>
2022-07-25 18:33:30 +0800

    add leave commad for bot

--- cogs/BasicCommands.py
+++ cogs/BasicCommands.py
@@ -11,16 +11,16 @@ from core.lib  import *
 class BasicCommands(CogTop):
 
     @commands.command()
-    async def help(self, ctx):
+    async def help(self, ctx:commands.Context):
         embed=nextcord.Embed(title="Lost 基本命令列表", color=0x0162b7) #blue
         embed.add_field(name="`e!help`", value="顯示命令列表", inline=True)
         embed.add_field(name="`e!ping`", value="顯示Lost 狀態", inline=True)
         embed.set_footer(text="Lost", icon_url=bot.icon_url)
 
-        await ctx.send(embed=embed)
+        await ctx.reply(embed=embed)
  
     @commands.command()
-    async def ping(self, ctx):
+    async def ping(self, ctx:commands.Context):
         delta_uptime = datetime.utcnow() - self.bot.launch_time
         hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
@@ -33,7 +33,7 @@ class BasicCommands(CogTop):
         embed.add_field(name="已運作時間", value=f"{days} d, {hours} h, {minutes} m, {seconds} s", inline=False)
         embed.set_footer(text="Lost", icon_url=bot.icon_url)
 
-        await ctx.send(embed=embed)
+        await ctx.reply(embed=embed)
     
         
 def setup(bot):
--- cogs/MusicCommands.py
+++ cogs/MusicCommands.py
@@ -1,4 +1,7 @@
-from pymysql import NULL
+from tabnanny import check
+from discord import Embed
+import requests
+import requests
 import wavelink
 import nextcord
 import bot
@@ -15,23 +18,30 @@ class MusicCommands(CogTop):
         self.bot = bot
         bot.loop.create_task(self.connect_nodes())
 
+    def check_node_status(self,url,http_code:int):
+        r = requests.get(url)
+        if r.status_code() == http_code:
+            return True
+        else:
+            return False
+
     async def connect_nodes(self):
         """連線到Lavalink節點"""
         await self.bot.wait_until_ready()
 
         await wavelink.NodePool.create_node(bot=self.bot,
-            host='185.187.170.159',
-            port=4873,
+            host='us01.lavalink.reload-dev.ml',
+            port=443,
             password='1A6DCEE7A27DA826B313FBC562CD2',
-            identifier='Public EU Main VPS 01',
-            region='eu_west',
+            identifier='Public US Main VPS 01',
+            https=True,
             )
+
         await wavelink.NodePool.create_node(bot=self.bot,
             host='51.161.130.134',
             port=10414,
             password='1A6DCEE7A27DA826B313FBC562CD2',
             identifier='Public Asia Main VPS 01',
-            region='singapore',
             )
         
         await wavelink.NodePool.create_node(bot=self.bot,
@@ -39,14 +49,12 @@ class MusicCommands(CogTop):
             port= 80,
             password='anything as a password',
             identifier='Public EU Main 01',
-            region='eu_west',
             )
         await wavelink.NodePool.create_node(bot=self.bot,
             host='lavalink.oops.wtf',
             port= 2000,
             password='www.freelavalink.ga',
             identifier='Public Asia Main 01',
-            region='singapore',
             )
 
     @commands.Cog.listener()
@@ -56,20 +64,31 @@ class MusicCommands(CogTop):
 
     @commands.command()
     async def play(self, ctx: commands.Context, *, search:wavelink.YouTubeTrack):
-        """Play a song with the given search query.
-        If not connected, connect to our voice channel.
-        """
+        await ctx.send('<:loading:1001057291036020776> 搜尋歌曲中...',delete_after=3)
+        
         if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            vc: wavelink.Player=await ctx.author.voice.channel.connect(cls=wavelink.Player)
         else:
-            vc: wavelink.Player = ctx.voice_client
+            vc: wavelink.Player=ctx.voice_client
+
+        playlist = wavelink.Queue()
 
         embed=nextcord.Embed(title=':white_check_mark: 已新增至播放清單 !')
         embed.add_field(name='%s'% (search.title),value='From Youtube',inline=False)
         embed.set_footer(text=f'Lost', icon_url=bot.icon_url)
-        
-        await vc.play(search)
-        await ctx.send(embed=embed)
+        await ctx.reply(embed=embed)
+
+        if playlist.is_empty:
+            await vc.play(search)
+        else:
+            await playlist.pop(search)
+
+    @commands.command()
+    async def leave(self,ctx:commands.Context):
+        vc: wavelink.Player=await ctx.voice_client.disconnect()
+        embed=nextcord.Embed(title=f':no_entry: 已被{ctx.author.name}要求中斷連線')
+        await ctx.reply(embed=embed)
+    
 
 def setup(bot):
     bot.add_cog(MusicCommands(bot))
