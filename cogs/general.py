d5cfcb698ff19103cec7a7608860541f0f950b91
EarthlyEric6 <minecaeft.creeper.eric@gmail.com>
2022-09-06 16:42:28 +0800

    add command info guild&user .

--- cogs/general.py
+++ cogs/general.py
@@ -1,16 +1,17 @@
 # -*- coding: UTF-8 -*-
+from unicodedata import name
 import nextcord
 import cogs.UI.dropmenu
 import psutil
-import datetime
 from datetime import datetime
 from core.utils import colors,icon,utils
 from nextcord.ext import commands
 from core.classes import Cogs
 from core.config import config
+from typing import Optional
 
 class General(Cogs):
-    @commands.command()
+    @commands.command(name='help')
     async def help(self, ctx:commands.Context):
         view=cogs.UI.dropmenu.HelpView()
 
@@ -21,7 +22,7 @@ class General(Cogs):
 
         await ctx.reply(embed=embed,view=view)
  
-    @commands.command()
+    @commands.command(name='ping')
     async def ping(self, ctx:commands.Context):
         bot_uptime = datetime.utcnow() - self.bot.launch_time
         hours, remainder = divmod(int(bot_uptime.total_seconds()), 3600)
@@ -49,6 +50,82 @@ class General(Cogs):
         embed.set_footer(text='Lost', icon_url=icon.icon_url)
 
         return await ctx.reply(embed=embed)
+
+    @commands.group(name='info')
+    async def info(self,ctx:commands.Context):
+        pass
+
+    @info.command(name='guild')
+    async def guild(self,ctx:commands.Context):
+        name=ctx.guild.name
+        icon_url=ctx.guild.icon.url
+        id=ctx.guild.id
+        members_count=ctx.guild.member_count
+        bot_count=len(ctx.guild.bots)
+        real_users_count=members_count-bot_count
+        owner_mention=ctx.guild.owner.mention
+        created_date=ctx.guild.created_at.strftime('%Y年%m月%d日 %H時%M分%S秒')
+        booster=ctx.guild.premium_subscription_count
+        channels_count=len(ctx.guild.channels)
+        text_channels_count=len(ctx.guild.text_channels)
+        voice_channels_count=len(ctx.guild.voice_channels)
+
+        if booster<2:
+            booster_level=0
+        elif booster>=2:
+            booster_level=1
+        elif booster>=7:
+            booster_level=2
+        elif booster>=14:
+            booster_level=3
+
+
+        embed=nextcord.Embed(
+        title='<:discord_api:1013700080118804580> |伺服器詳情',
+        description='***%s***'%(name),
+        color=colors.purple,
+        timestamp=datetime.now())
+        embed.set_thumbnail(url=icon_url)
+        embed.add_field(name='伺服器ID',value='`%s`'%(id),inline=False)
+        embed.add_field(name='創建時間',value='`%s`'%(created_date),inline=False)
+        embed.add_field(name='總人數',value='`%s(使用者:%s，機器人:%s)`'%(
+            members_count,
+            real_users_count,
+            bot_count
+            ),inline=False)
+        embed.add_field(name='加成狀態',value='`%d個`/`%d級`'%(booster,booster_level),inline=False)
+        embed.add_field(name='總頻道數',value='`%s個`\n`(文字頻道:%s個，語音頻道:%s個)`\n(機器人僅顯示有權訪問)\n'%(channels_count,text_channels_count,voice_channels_count))
+        embed.add_field(name='擁有者',value='%s'%(owner_mention),inline=False)
+        embed.set_footer(text='Lost', icon_url=icon.icon_url)
+
+        return await ctx.reply(embed=embed)
+    
+    @info.command(name='user')
+    async def user(self,ctx:commands.Context,user:nextcord.Member=None):
+        if user==None:
+            name=ctx.author.name
+            avatar_url=ctx.author.avatar.url
+            id=ctx.author.id
+            created_date=ctx.author.created_at.strftime('%Y年%m月%d日 %H時%M分%S秒')
+        else:
+            name=user.name
+            id=user.id
+            avatar_url=user.avatar.url
+            created_date=user.created_at.strftime('%Y年%m月%d日 %H時%M分%S秒')
+
+        embed=nextcord.Embed(
+        title='<:discord_api:1013700080118804580> |使用者詳情',
+        description='***%s***'%(name),
+        color=colors.purple,
+        timestamp=datetime.now())
+        embed.set_thumbnail(url=avatar_url)
+        embed.add_field(name='`使用者ID`',value='%s'%(id),inline=False)
+        embed.add_field(name='創建時間',value='`%s`'%(created_date),inline=False)
+        embed.set_footer(text='Lost', icon_url=icon.icon_url)
+
+        await ctx.reply(embed=embed)
+        
+    
      
 def setup(bot):
     bot.add_cog(General(bot))
