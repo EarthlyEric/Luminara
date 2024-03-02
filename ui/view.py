76d2d0e9df6954595262fd75e0491e9f4a8765bf
EarthlyEric <earthlyeric6@gmail.com>
2024-03-02 11:08:29 +0800

    update discord.py

--- ui/view.py
+++ ui/view.py
@@ -1,12 +1,12 @@
 # -*- coding: UTF-8 -*-
-from nextcord import Emoji
-import nextcord
+from discord import Emoji
+import discord
 
-class Help(nextcord.ui.Select):
+class Help(discord.ui.Select):
     def __init__(self):
         options=[
-            nextcord.SelectOption(label='一般',value='general',description='基礎指令',emoji='<:general:1002820067324600380>'),
-            nextcord.SelectOption(label='音樂',value='music',description="Let's start the music !",emoji='<:music:1002824345095241878>')
+            discord.SelectOption(label='一般',value='general',description='基礎指令',emoji='<:general:1002820067324600380>'),
+            discord.SelectOption(label='音樂',value='music',description="Let's start the music !",emoji='<:music:1002824345095241878>')
         ]
         super().__init__(
             placeholder='指令指南',
@@ -14,14 +14,14 @@ class Help(nextcord.ui.Select):
             max_values=1,
             options=options,
             )
-        async def callback(self,interaction:nextcord.Interaction):
+        async def callback(self,interaction:discord.Interaction):
             value=self.values[0]
             if value=='general':
                 return await interaction.response.send_message('尚未完成')
             elif value=='music':
                 return await interaction.response.send_message('尚未完成')
 
-class HelpView(nextcord.ui.View):
+class HelpView(discord.ui.View):
     def __init__(self):
         super().__init__()
         self.add_item(Help())
