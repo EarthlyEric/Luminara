db035b659b2bf746c8737438cf22a57494df5e1c
EarthlyEric <earthlyeric6@gmail.com>
2024-03-07 20:55:17 +0800

    setup logging

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 import discord
 import wavelink
+import logging
 from discord.ext import commands
 
 from core.config import config
@@ -9,10 +10,12 @@ class theBot(commands.Bot):
     def __init__(self) -> None:
         intents: discord.Intents = discord.Intents.all()
         intents.message_content = True
+
         super().__init__(command_prefix=config.commandPrefix, intents=intents)
 
     async def setup_hook(self) -> None:
         nodes = [wavelink.Node(identifier="Self-Hosted Node US 01",uri=config.lavalinkHost, password=config.lavalinkPasswd)]
+
         await wavelink.Pool.connect(nodes=nodes, client=self, cache_capacity=100)
     
     async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload) -> None:
