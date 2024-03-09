2724c5a8b7873e458183adee31b26ef0682a7286
EarthlyEric <earthlyeric6@gmail.com>
2024-03-09 21:38:45 +0800

    Add voice state update listener and track end listener

--- cogs/commands/music.py
+++ cogs/commands/music.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+import asyncio
 import discord
 import wavelink
 from datetime import datetime, timezone
@@ -14,7 +15,19 @@ from ui.musiccontroller import MusicController
 class Music(Cogs):
     def __init__(self, bot):
         super().__init__(bot)
+    
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState) -> None:
+        player: wavelink.Player | None = await self.bot.get_player(member.guild)
+        if not player:
+            return
 
+        if len(player.channel.members) == 0 and player.connected:
+            await player.home.channel.send("沒有人在語音頻道中，將在 10 秒後自動離開 !")
+            await asyncio.sleep(10)
+            return await player.disconnect()
+
+    
     @commands.Cog.listener()
     async def on_wavelink_track_start(self, payload: wavelink.TrackStartEventPayload) -> None:
         player: wavelink.Player | None = payload.player
@@ -30,7 +43,7 @@ class Music(Cogs):
         embed.color = discord.Color.purple()
         embed.timestamp=datetime.now(timezone.utc)
         embed.description = f"**{track.title}** by `{track.author}`"
-        embed.set_footer(text='Luminara')
+        embed.set_footer(text="Luminara")
         
         if track.artwork:
             embed.set_thumbnail(url=track.artwork)
@@ -44,39 +57,39 @@ class Music(Cogs):
         await player.home.send(embed=embed)
 
         musciController =MusicController()
-    
+
     @commands.Cog.listener()
-    async def on_wavelink_truck_end(self, payload: wavelink.TrackEndEventPayload) -> None:
+    async def on_wavelink_track_end(self, payload: wavelink.TrackEndEventPayload) -> None:
         player: wavelink.Player | None = payload.player
         if not player:
             # Handle edge cases...
             return
 
-        if not player.queue:
+        if player.queue.is_empty:
             pass
+        elif len(player.channel.members) == 0:
+            pass
+            #
         else:
             return await player.play(player.queue.get())
-
-        
-
     
-    @commands.command(name='play')
+    @commands.command(name="play")
     async def play(self,ctx: commands.Context, *, query: str):
         embed = discord.Embed()
-        embed.set_footer(text='Luminara')
+        embed.set_footer(text="Luminara")
         embed.timestamp=datetime.now(timezone.utc)
         if not ctx.guild:
             embed.color=discord.Color.red()
             embed.title="%s | 無法在私人訊息中使用此命令 !" % (emojis.errors)
             return await ctx.send(embed=embed)
         
-        loading= await ctx.send("%s | 正在處理您的請求，請稍後..." % (emojis.loading))
+        loading = await ctx.send("%s | 正在處理您的請求，請稍後..." % (emojis.loading))
     
         player : wavelink.Player
         player = cast(wavelink.Player, ctx.voice_client)
         
         if not player:
-            loading.delete()
+            await loading.delete(delay=1.5)
             try:
                 embed.color=colors.green
                 embed.title="%s | 成功加入您所處的語音頻道 %s !" % (emojis.success,ctx.author.voice.channel.mention)
@@ -85,7 +98,7 @@ class Music(Cogs):
                 await ctx.send(embed=embed)
             except AttributeError:
                 embed.color=discord.Color.red()
-                embed.title="%s | 請先加入任一語音頻道在使用本命令 !" % (emojis.errors)
+                embed.title="%s | 請先加入任一語音頻道後，再執行本命令 !" % (emojis.errors)
                 return await ctx.send(embed=embed)   
             except discord.ClientException:
                 embed.color=discord.Color.red()
@@ -94,11 +107,11 @@ class Music(Cogs):
             
         player.autoplay = wavelink.AutoPlayMode.enabled
 
-        if not hasattr(player, 'home'):
+        if not hasattr(player, "home"):
             player.home = ctx.channel
         elif player.home != ctx.channel:
             embed.color=discord.Color.red()
-            embed.title="%s | 只能在 %s 使用本命令 !" % (emojis.errors,player.home.mention)
+            embed.title="%s | 只能在 %s 執行本命令 !" % (emojis.errors,player.home.mention)
             return await ctx.send(embed=embed)
         
         tracks: wavelink.Search = await wavelink.Playable.search(query)
@@ -119,7 +132,7 @@ class Music(Cogs):
             await ctx.send(f"已將 **`{track}`** 加入播放清單")
 
         if not player.playing:
-            # Play now since we aren't playing anything...
+            # Play now since we aren"t playing anything...
             await player.play(player.queue.get())
             
 
