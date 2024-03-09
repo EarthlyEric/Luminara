bd2f84bb393a509dca144f87cfc90462b45ac12c
EarthlyEric <earthlyeric6@gmail.com>
2024-03-09 20:29:05 +0800

    Add title and timestamp to MusicController constructor

--- ui/musiccontroller.py
+++ ui/musiccontroller.py
@@ -1,7 +1,17 @@
 import discord
+import datetime
 
 class MusicController(discord.Embed):
-    def __init__(self):
+    def __init__(
+            self,
+            title:str,
+            timestamp:datetime.datetime,
+            
+            ):
         super().__init__()
+        self.title=title
+        self.timestamp=timestamp
         self.color=discord.Color.blurple()
+        self.set_author(name='音樂播放控制器',icon_url='https://cdn.discordapp.com/emojis/1002824345095241878.png')
+        self.set_footer(text='Luminara')
         
