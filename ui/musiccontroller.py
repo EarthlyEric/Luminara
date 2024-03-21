03e20175f25a041c977cff726f225a9039283493
EarthlyEric <earthlyeric6@gmail.com>
2024-03-21 22:53:55 +0800

    Update MusicController to include track artwork and emojis

--- ui/musiccontroller.py
+++ ui/musiccontroller.py
@@ -2,7 +2,7 @@ import discord
 import datetime
 import wavelink
 
-from core.utils import utils
+from core.utils import utils,emojis
 
 class MusicController(discord.Embed):
     def __init__(
@@ -11,9 +11,11 @@ class MusicController(discord.Embed):
             track: wavelink.Playable
             ):
         super().__init__()
-        self.title=f" **{track.title}** - `{track.author}`"
-        self.description=f"```[{utils.convertMiliseconds(track.length)}]```"
+        self.title=f"{emojis.notes} | **{track.title}** - `{track.author}`"
+        self.description=f"`[{utils.convertMiliseconds(track.length)}/]``"
         self.url=track.uri
+        if track.artwork:
+            self.thumbnail = track.artwork
         self.timestamp=timestamp
         self.color=discord.Color.blurple()
         self.set_footer(text="Luminara")
