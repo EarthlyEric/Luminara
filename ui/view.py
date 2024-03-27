a6386bcc48f487b9e8eb896df207eeb3f4be65ef
EarthlyEric <earthlyeric6@gmail.com>
2024-03-27 23:39:52 +0800

    Refactor UI view.py

--- ui/view.py
+++ ui/view.py
@@ -24,7 +24,8 @@ class Help(discord.ui.Select):
 class HelpView(discord.ui.View):
     def __init__(self):
         super().__init__()
-        self.add_item(Help())
+        
+    
 
 class PlayButton(discord.ui.Button):
     def __init__(self):
