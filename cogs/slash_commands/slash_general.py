d263bc0afde361bdbde6b58b7f2f96b923e10e57
EarthlyEric <earthlyeric6@gmail.com>
2024-03-27 23:39:18 +0800

    Add help command and reload command to slash_general.py and slash_debugs.py

--- cogs/slash_commands/slash_general.py
+++ cogs/slash_commands/slash_general.py
@@ -9,4 +9,13 @@ from ui.view import *
 from core.utils import colors,icon,utils,emojis
 
 class slash_General(Slash_Cogs):
-    pass
+    @commands.command(name="help")
+    async def help(self, ctx: commands.Context):
+        view=HelpView()
+        
+        embed=discord.Embed(color=colors.purple,timestamp=datetime.now())
+        embed.set_author(name="Luminara使用指南",icon_url=icon.guide_icon_url,url="https://blog.earthlyeric6.ml/")
+        embed.add_field(name="Hello，我是Luminara，很高興見到你!",value="你可以從下面選擇想看的指令使令用法類別。")
+        embed.set_footer(text="Luminara")
+
+        return await ctx.reply(embed=embed,view=view)
--- cogs/slash_debugs.py
+++ cogs/slash_debugs.py
@@ -4,45 +4,45 @@ from discord.ext import commands
 from datetime import datetime
 
 from classes import Slash_Cogs
-from core.utils import colors,icon,emojis
+from core.utils import colors, icon, emojis
 
 class Slash_Debugs(Slash_Cogs):
-    extension_location_list={
-        "general":"commands",
-        "music":"commands",
-        "management":"commands",
-        "errors":"events",
-        "events":"events",
-        "tasks":"events"}
-    @discord.app_commands.command(name="reload",description="重新載入指定的Extension")
+    extension_location_list = {
+        "general": "commands",
+        "music": "commands",
+        "management": "commands",
+        "errors": "events",
+        "events": "events",
+        "tasks": "events"
+    }
+
+    @discord.app_commands.command(name="reload", description="重新載入指定的Extension")
     @discord.app_commands.choices(extensions=[
-        discord.app_commands.Choice(name="general",value="general"),
-        discord.app_commands.Choice(name="management",value="management"),
-        discord.app_commands.Choice(name="music",value="music"),
-        discord.app_commands.Choice(name="errors",value="errors"),
-        discord.app_commands.Choice(name="events",value="events"),
-        discord.app_commands.Choice(name="tasks",value="tasks")
+        discord.app_commands.Choice(name="general", value="general"),
+        discord.app_commands.Choice(name="management", value="management"),
+        discord.app_commands.Choice(name="music", value="music"),
+        discord.app_commands.Choice(name="errors", value="errors"),
+        discord.app_commands.Choice(name="events", value="events"),
+        discord.app_commands.Choice(name="tasks", value="tasks")
     ])
     @commands.is_owner()
-    async def reload(self,interaction: discord.Interaction,extensions: discord.app_commands.Choice[str] ):
-            extension=extensions.value.replace(""","")
-            extension_location=self.extension_location_list[f"{extension}"]
-            try:
-                self.bot.reload_extension("cogs.%s.%s"%(extension_location,extension))
-                print(f"[Debug][INFO] Reloaded {extension_location}.{extension}")
-                embed = discord.Embed(title="重新載入中...", description="`%s` 載入成功"%(extension), color=colors.green,timestamp=datetime.now())
-                embed.set_footer(text="Luminara•Debug System", icon_url=icon.icon_url)
-                return await interaction.response.send_message(embed=embed)
-            except Exception as e:
-                print("[Debug][ERROR] %s")%(e)
-                embed = discord.Embed(title="重新載入中...", description="載入失敗", color=colors.red,timestamp=datetime.now())
-                embed.add_field(name="原因",value="`%s`"%(e))
-                embed.set_footer(text="Luminara", icon_url=icon.icon_url)
-                embed.set_footer(text="Luminara•Debug System", icon_url=icon.icon_url)
-                return await interaction.response.send_message(embed=embed)
-        
-        
-        
+    async def reload(self, interaction: discord.Interaction, extensions: discord.app_commands.Choice[str]):
+        extension = extensions.value.replace('"', '')
+        extension_location = self.extension_location_list[f"{extension}"]
+        try:
+            self.bot.reload_extension(f"cogs.{extension_location}.{extension}")
+            print(f"[Debug][INFO] Reloaded {extension_location}.{extension}")
+            embed = discord.Embed(title="重新載入中...", description=f"`{extension}` 載入成功", color=colors.green, timestamp=datetime.now())
+            embed.set_footer(text="Luminara•Debug System", icon_url=icon.icon_url)
+            return await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            print(f"[Debug][ERROR] {e}")
+            embed = discord.Embed(title="重新載入中...", description="載入失敗", color=colors.red, timestamp=datetime.now())
+            embed.add_field(name="原因", value=f"`{e}`")
+            embed.set_footer(text="Luminara", icon_url=icon.icon_url)
+            embed.set_footer(text="Luminara•Debug System", icon_url=icon.icon_url)
+            return await interaction.response.send_message(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Slash_Debugs(bot))
