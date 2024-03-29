a923543edaa09d58f6df01a18874fc0e7e935245
EarthlyEric <earthlyeric6@gmail.com>
2024-03-29 23:20:58 +0800

    Refactor music controller and help view

--- ui/musiccontroller.py
+++ ui/musiccontroller.py
@@ -1,24 +1,24 @@
+# -*- coding: UTF-8 -*-
 import discord
-import datetime
 import wavelink
 
-from core.utils import utils,emojis
+import classes
+from core.utils import emojis
 
-class MusicController(discord.Embed):
-    def __init__(
-            self,
-            timestamp:datetime.datetime,
-            track: wavelink.Playable
-            ):
-        super().__init__()
-        self.title=f"{emojis.notes} | **{track.title}** - `{track.author}`"
-        self.description=f"`[{utils.convertMiliseconds(track.length)}/]`"
-        self.url=track.uri
-        if track.artwork:
-            self.set_thumbnail(url=track.artwork) 
-        self.timestamp=timestamp
-        self.color=discord.Color.blurple()
-        self.set_footer(text="Luminara")
+class MusicControllerView(discord.ui.View):
+    def __init__(self, player:wavelink.Player, bot: classes.theBot,timeout=180):
+        super().__init__(timeout=timeout)
+        self.bot = bot
+        self.player = player
 
-        super.__init__(self)
+    @discord.ui.button(label="Skip", style=discord.ButtonStyle.blurple)
+    async def skip(self, button: discord.ui.Button, interaction: discord.Interaction):
+        if self.player.queue.is_empty:
+            return await interaction.response.send_message(f"{emojis.errors} | 播放序列已經空了，無法進行操作!")
+        elif self.player.paused:
+            return await interaction.response.send_message(f"{emojis.errors} | 播放暫停中，無法進行操作!")   
+        elif self.player.playing:
+            await interaction.response.send_message(":track_next: | 已跳過當前曲目 !即將播放下一首曲目。")
+            return await self.player.play(self.player.queue.get())
+        
         
