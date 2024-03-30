de823972dea4eb84e405f7fba08ccf4c950ad26a
EarthlyEric <earthlyeric6@gmail.com>
2024-03-31 00:55:06 +0800

    Refactor command names and add app command support

--- cogs/commands/management.py
+++ cogs/commands/management.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+from typing import Optional
 import discord
 from datetime import datetime
 from discord.ext import commands
@@ -8,17 +9,14 @@ from ui.view import *
 from core.utils import colors,icon,emojis
 
 class Management(Cogs):
-    @commands.group(name="info")
-    async def info(self,ctx: commands.Context):
-        pass
 
-    @info.command(name="guild")
-    async def guild(self,ctx: commands.Context):
+    @commands.hybrid_command(name="guild-info",description="顯示伺服器詳情",with_app_command=True)
+    async def guild_info(self,ctx: commands.Context):
         name=ctx.guild.name
         icon_url=ctx.guild.icon.url
         id=ctx.guild.id
         members_count=ctx.guild.member_count
-        bot_count=len(ctx.guild.bots)
+        bot_count=len([member for member in ctx.guild.members if member.bot])
         real_users_count=members_count-bot_count
         owner_mention=ctx.guild.owner.mention
         created_date=ctx.guild.created_at.strftime("%Y年%m月%d日 %H時%M分%S秒")
@@ -36,11 +34,9 @@ class Management(Cogs):
         elif booster>=14:
             booster_level=3
 
-        embed=discord.Embed(
-        title="<:discord_api:1013700080118804580> | 伺服器詳情",
-        description="***%s***"%(name),
-        color=colors.purple,
-        timestamp=datetime.now())
+        embed=discord.Embed(color=colors.purple, timestamp=datetime.now())
+        embed.title="%s | 伺服器詳情"%(emojis.server)
+        embed.description="***%s***"%(name)
         embed.set_thumbnail(url=icon_url)
         embed.add_field(name="伺服器ID",value="`%s`"%(id),inline=False)
         embed.add_field(name="創建時間",value="`%s`"%(created_date),inline=False)
@@ -52,13 +48,13 @@ class Management(Cogs):
         embed.add_field(name="加成狀態",value="`%d個`/`%d級`"%(booster,booster_level),inline=False)
         embed.add_field(name="總頻道數",value="`%s個`\n`(文字頻道:%s個，語音頻道:%s個)`\n(機器人僅顯示有權訪問)\n"%(channels_count,text_channels_count,voice_channels_count))
         embed.add_field(name="擁有者",value="%s"%(owner_mention),inline=False)
-        embed.set_footer(text="Luminara", icon_url=icon.icon_url)
+        embed.set_footer(text="Luminara")
 
         return await ctx.reply(embed=embed)
     
-    @info.command(name="user")
-    async def user(self,ctx: commands.Context,user:discord.Member=None):
-        if user==None:
+    @commands.hybrid_command(name="user-info",description="顯示使用者詳情",with_app_command=True)
+    async def user_info(self,ctx: commands.Context,user:Optional[discord.User]):
+        if not user:
             name=ctx.author.name
             avatar_url=ctx.author.avatar.url
             id=ctx.author.id
@@ -69,15 +65,13 @@ class Management(Cogs):
             avatar_url=user.avatar.url
             created_date=user.created_at.strftime("%Y年%m月%d日 %H時%M分%S秒")
 
-        embed=discord.Embed(
-        title="%s | 使用者詳情"%(emojis.discord_api),
-        description="***%s***"%(name),
-        color=colors.purple,
-        timestamp=datetime.now())
+        embed=discord.Embed(color=colors.purple, timestamp=datetime.now())
+        embed.title="%s | 使用者詳情"%(emojis.user)
+        embed.description="***%s***"%(name)
         embed.set_thumbnail(url=avatar_url)
         embed.add_field(name="`使用者ID`",value="%s"%(id),inline=False)
-        embed.add_field(name="創建時間",value="`%s`"%(created_date),inline=False)
-        embed.set_footer(text="Luminara", icon_url=icon.icon_url)
+        embed.add_field(name="帳號創建時間",value="`%s`"%(created_date),inline=False)
+        embed.set_footer(text="Luminara")
 
         return await ctx.reply(embed=embed)
 
